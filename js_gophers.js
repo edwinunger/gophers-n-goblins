258a0c3505c7d3a0d15a2b6aa001700bf60f2c49
Edwin Unger & Guilherme Sá <pair+edwinunger.guilsa@devbootcamp.com>
2015-05-08 15:02:12 -0700

    string, row

--- js_gophers.js
+++ js_gophers.js
@@ -3,21 +3,45 @@ Cell = function(str_index, str){
   this.str = str;
   this.column = parseInt(str_index) % 7;
   this.row = Math.floor(parseInt(str_index) / 7);
+  this.leftDiag = 5;
+  this.rightDiag = 5;
 };
 
-Cell.prototype = function(){
-};
+// game.board[0].leftDiag = 0
+// leftDiag1
+// leftDiag2
+// leftDiag3
+// leftDiag4
+// leftDiag5
+// leftDiag6
 
-// Cell.prototype.get_row = function(){
-// };
+// rightDiag1
+// rightDiag2
+// rightDiag3
+// rightDiag4
+// rightDiag5
+// rightDiag6
 
-// Cell.prototype.get_column = function(){
-// };
 Board = function(board_string){
   this.board = [];
+  this.currentTurn = 0;
   for (var i = 0; i < board_string.length; i++) {
     str = board_string[i];
     this.board.push(new Cell(i, str));
+  }
+};
+
+Board.prototype.changePlayer = function() {
+  this.currentTurn = ((this.currentTurn + 1) % 2);
+};
+
+// NEEDS TESTING
+Board.prototype.getRow = function(row) {
+  this.rowArray = [];
+  for(i=41; i>=0; i--) {
+    if (this.board[i].row === row) {
+      this.rowArray.push(this.board[i]);
+    }
   }
 };
 
@@ -28,33 +52,41 @@ Board.prototype.getColumn = function(column) {
       this.columnArray.push(this.board[i]);
     }
   }
-  this.columnDrop();
+  this.columnDrop(this.currentTurn);
 };
 
 Board.prototype.countUp = 0;
 
-Board.prototype.columnDrop = function() {
-  if (this.columnArray[this.countUp].str !== "x") {
-    this.columnArray[this.countUp].str = "x";
+Board.prototype.columnDrop = function(player_num) {
+  var players = ["x", "o"];
+  if ((this.columnArray[this.countUp].str !== "-") && (this.columnArray[this.countUp].row === 0)) {
+    console.log('Error: Please try another column.');
+  }
+  else if (this.columnArray[this.countUp].str === "-") {
+    // debugger
+    this.columnArray[this.countUp].str = players[player_num];
     this.countUp = 0;
   }
-  else if (this.columnArray[this.countUp].str === "x") {
+  else if (this.columnArray[this.countUp].str !== "-") {
     this.countUp++;
-    this.columnDrop();
+    this.columnDrop(player_num);
   }
 };
 
-Board.prototype.to_s = function(){
-  for (var i = 0; i < this.board.length; i++) {
-    for (var y = 0; y < 6; y++) {
-      console.log(this.board[i].str)
-    }
-    console.log("\n")
-    // console.log("String: " + this.board[i].str);
-    // console.log("Column: " + this.board[i].column)
-    // console.log("Row: " + this.board[i].row)
+Board.prototype.leftDiag = function() {
+  for (i=0; i<=41; i+=8) {
+    this.board[i].leftDiag = 0;
   }
-}
+};
+
+Board.prototype.getLeftDiag = function() {
+  this.leftDiagArray = [];
+  for(i=41; i >= 0; i--) {
+    if (this.board[i].leftDiag === 0) {
+      this.leftDiagArray.push(this.board[i]);
+    }
+  }
+};
 
 
 
@@ -75,18 +107,28 @@ Board.prototype.to_s = function(){
 // };
 
 
+
 game = new Board("------------------------------------------");
 // game.nextAvailable(5);
 // game.play(1, 5)
 // game.play(2, 6)
 // game.player1(0)
-game.getColumn(0);
-game.getColumn(1);
+game.getColumn(0); // selects column number and puts all cells into an array of that column
+// also calls columnDrop, which accepts player number and drops into column based on player id
+// game.getColumn(0);
 // game.to_s();
 
-console.log("-") console.log("-")
+game.changePlayer();
+game.getColumn(0);
+game.changePlayer();
+game.getColumn(0);
+game.changePlayer();
+game.getColumn(0);
 
 
+
+game.leftDiag;
+// game.getLeftDiag;
 
 // game.player2
 
